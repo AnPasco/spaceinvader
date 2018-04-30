f1032afb52828b81a6fb981d3bd526893000f719
Antoine <Antoine@164.81.228.235>
2018-04-30 17:27:43 +0200

    phase 2

--- src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
@@ -1,23 +1,46 @@
 package fr.unilim.iut.spaceinvader;
 
 public class SpaceInvaders {
-    int longueur;
-    int hauteur;
+	int longueur;
+	int hauteur;
+	Vaisseau vaisseau;
 
-    public SpaceInvaders(int longueur, int hauteur) {
-	   this.longueur = longueur;
-	   this.hauteur = hauteur;
-   }
-    
+	public SpaceInvaders(int longueur, int hauteur) {
+		this.longueur = longueur;
+		this.hauteur = hauteur;
+	}
+
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		this.vaisseau = new Vaisseau(x, y);
+	}
+
+	private char recupererMarqueDeLaPosition(int y, int x) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+			marque = 'V';
+		else
+			marque = '.';
+		return marque;
+	}
+
+	private boolean aUnVaisseauQuiOccupeLaPosition(int y, int x) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau != null;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append('.');
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
 			}
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
 	}
+
 }
