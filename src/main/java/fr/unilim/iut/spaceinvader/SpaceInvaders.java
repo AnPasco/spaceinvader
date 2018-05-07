6f157df298465db3aaed430a67b249ae142c4927
Antoine <Antoine@164.81.228.235>
2018-05-07 16:37:18 +0200

    phase 2.2.2 - Refactoring

--- src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvader/SpaceInvaders.java
@@ -17,12 +17,14 @@ public class SpaceInvaders {
 
 	public void positionnerUnNouveauVaisseau(int x, int y) {
 
-		if (x >= longueur) {
-			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu !");
-		} else {
-			vaisseau = new Vaisseau(x, y);
-		}
+		if (!estDansEspaceJeu(x, y))
+			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
 
+		vaisseau = new Vaisseau(x, y);
+	}
+
+	private boolean estDansEspaceJeu(int x, int y) {
+		return ((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur));
 	}
 
 	private char recupererMarqueDeLaPosition(int y, int x) {
