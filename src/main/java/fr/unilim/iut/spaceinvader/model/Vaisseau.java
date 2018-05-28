afb8d9a8114e34cfb792f2a11738ffd6dcb59ea9
Antoine <Antoine@164.81.228.235>
2018-05-28 17:22:29 +0200

    Phase 5.2.1 - ajout erreur verif

--- src/main/java/fr/unilim/iut/spaceinvader/model/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvader/model/Vaisseau.java
@@ -1,5 +1,7 @@
 package fr.unilim.iut.spaceinvader.model;
 
+import fr.unilim.iut.spaceinvader.utils.MissileException;
+
 public class Vaisseau extends Sprite {
 	
 	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
@@ -7,13 +9,20 @@ public class Vaisseau extends Sprite {
 	}
 
 	public Missile tirerUnMissile(Dimension dimensionMissile, int vitesseMissile) {
+		if(this.dimension.longueur<dimensionMissile.longueur) {
+			throw new MissileException("Erreur !");
+		}else {
+			Position positionOrigineMissile = calculerLaPositionDeTirDuMissile(dimensionMissile);
+			return new Missile(dimensionMissile, positionOrigineMissile,  vitesseMissile);
+		}
+	}
 
+	private Position calculerLaPositionDeTirDuMissile(Dimension dimensionMissile) {
 		int abscisseMilieuVaisseau = this.abscisseLaPlusAGauche() + (this.longueur() / 2);
 		int abscisseOrigineMissile = abscisseMilieuVaisseau - (dimensionMissile.longueur() / 2);
 
 		int ordonneeeOrigineMissile = this.ordonneeLaPlusBasse() - 1;
 		Position positionOrigineMissile = new Position(abscisseOrigineMissile, ordonneeeOrigineMissile);
-
-		return new Missile(dimensionMissile, positionOrigineMissile,  vitesseMissile);
+		return positionOrigineMissile;
 	}
 }
